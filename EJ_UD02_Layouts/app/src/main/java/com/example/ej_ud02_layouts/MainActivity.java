a5785bdab27e7f5699c05d197450bd3b663be1e5
mario <marioiram2000@gmail.com>
2021-09-27 11:17:40 +0200

    Commit

---
+++ EJ_UD02_Layouts/app/src/main/java/com/example/ej_ud02_layouts/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ej_ud02_layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
