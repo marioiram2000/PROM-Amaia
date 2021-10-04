a335d1fdbed17d41b97b17e3848e0a3502f76a14
mario <marioiram2000@gmail.com>
2021-10-04 11:56:05 +0200

    cb2

---
+++ EJ_UD02_ControlesBasicos_2/app/src/main/java/com/example/ej_ud02_controlesbasicos_2/actividad01_C.java
@@ -0,0 +1,14 @@
+package com.example.ej_ud02_controlesbasicos_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class actividad01_C extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad01_c);
+    }
+}
