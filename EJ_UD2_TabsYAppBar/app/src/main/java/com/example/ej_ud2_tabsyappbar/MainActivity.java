b523d95607384d0a417c47a51a881a4ba14d3a9a
mario <marioiram2000@gmail.com>
2021-10-27 10:10:07 +0200

    ejercicios

--- EJ_UD2_TabsYAppBar/app/src/main/java/com/example/ej_ud2_tabsyappbar/MainActivity.java
+++ EJ_UD2_TabsYAppBar/app/src/main/java/com/example/ej_ud2_tabsyappbar/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ej_ud2_tabsyappbar;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
@@ -10,7 +11,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -22,73 +22,80 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ViewPager viewPager = findViewById(R.id.viewpager);
-        viewPager.setAdapter(new PageAdaprter());
+        viewPager.setAdapter(new TabAdapter());
 
-        TabLayout tabLayout= findViewById(R.id.tabLayout);
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
         tabLayout.setTabMode(TabLayout.MODE_FIXED);
         tabLayout.setupWithViewPager(viewPager);
-    }
-}
 
-class PageAdaprter extends PagerAdapter {
-    private LinearLayout pestana1;
-    private LinearLayout pestana2;
-    private LinearLayout pestana3;
-    private final int[] pestanas ={R.string.tab1, R.string.tab2,
-            R.string.tab3 };
-    @Override
-    public int getCount() {
-        return 3;
+
     }
-    @Nullable
-    @Override
-    public CharSequence getPageTitle(int position) {
-        String s = getString(pestanas[position]);
-        return s;
-    }
-    @NonNull
-    @Override
-    public Object instantiateItem(@NonNull ViewGroup container,
-                                  int position) {
-        View page;
-        switch (position){
-            case 0:
-                if (pestana1 == null){
-                    pestana1 = (LinearLayout)
-                            LayoutInflater.from(PestanasConViewPager.this)
-                                    .inflate(R.layout.pestana1, container,false );
-                }
-                page= pestana1;
-                break;
-            case 1:
-                if (pestana2 == null) {
-                    pestana2 = (LinearLayout)
-                            LayoutInflater.from(PestanasConViewPager.this)
-                                    .inflate(R.layout.pestana2,container,false);
-                }
-                page=pestana2;
-                break;
-            default:
-                if (pestana3 == null) {
-                    pestana3 = (LinearLayout)
-                            LayoutInflater.from(PestanasConViewPager.this)
-                                    .inflate(R.layout.pestana3,container,false);
-                }
-                page=pestana3;
-                break;
+
+
+    class TabAdapter extends PagerAdapter {
+        private ConstraintLayout pestana1;
+        private ConstraintLayout pestana2;
+        private ConstraintLayout pestana3;
+        private final int[] pestanas = {R.string.tab1, R.string.tab2, R.string.tab3};
+
+        @Override
+        public int getCount() {
+            return 3;
         }
-        container.addView(page, 0);
-        return page;
-    }
-    @Override
-    public boolean isViewFromObject(@NonNull View view,
-                                    @NonNull Object object) {
-        //return false;
-        return view == object;
-    }
-    @Override
-    public void destroyItem(@NonNull ViewGroup container,
-                            int position, @NonNull Object object) {
-        container.removeView((View)object);
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return getString(pestanas[position]);
+        }
+
+
+        @NonNull
+        @Override
+        public Object instantiateItem(@NonNull ViewGroup container,
+                                      int position) {
+            View page;
+            switch (position) {
+                case 0:
+                    if (pestana1 == null) {
+                        pestana1 = (ConstraintLayout)
+                                LayoutInflater.from(MainActivity.this)
+                                        .inflate(R.layout.pestana_chats, container, false);
+                    }
+                    page = pestana1;
+                    break;
+                case 1:
+                    if (pestana2 == null) {
+                        pestana2 = (ConstraintLayout)
+                                LayoutInflater.from(MainActivity.this)
+                                        .inflate(R.layout.pestana_contactos, container, false);
+                    }
+                    page = pestana2;
+                    break;
+                default:
+                    if (pestana3 == null) {
+                        pestana3 = (ConstraintLayout)
+                                LayoutInflater.from(MainActivity.this)
+                                        .inflate(R.layout.pestana_llamadas, container, false);
+                    }
+                    page = pestana3;
+                    break;
+            }
+            container.addView(page, 0);
+            return page;
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view,
+                                        @NonNull Object object) {
+            //return false;
+            return view == object;
+        }
+
+        @Override
+        public void destroyItem(@NonNull ViewGroup container,
+                                int position, @NonNull Object object) {
+            container.removeView((View) object);
+        }
     }
 }
