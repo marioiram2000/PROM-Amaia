71a5f0e522c7f4278ac77a440224c02857752841
mario <marioiram2000@gmail.com>
2021-11-24 09:23:04 +0100

    minor changes

---
+++ EJ_UD03_Android_Ficheros/app/src/main/java/com/example/ej_ud03_android_ficheros/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.ej_ud03_android_ficheros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void aniadirFichInt(View view){
+
+    }
+
+    public void aniadirFichExt(View view){
+
+    }
+
+    public void leerFichInt(View view){
+
+    }
+
+    public void leerFichExt(View view){
+
+    }
+
+    public void leerRecurso(View view){
+
+    }
+
+    public void borrarFichInt(View view){
+
+    }
+
+    public void borrarFichExt(View view){
+
+    }
+}
